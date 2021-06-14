f2f1396aed2492d3516d9e5ea04e8717e5191e07
fachrighiffary <fachrighiffary@gmail.com>
2021-06-14 14:30:24 +0700

    merge from fachri

---
+++ src/fri_Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from "./style.css";
+
+const Home = () => {
+  return (
+    <div className="header">
+      <p>Ini adalah home buatan Fachri</p>
+    </div>
+  );
+};
+
+export default Home;
